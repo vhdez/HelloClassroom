a211a2e99e1240d9032f26e0fe667e0219ae7707
Mr. Hernandez <mrhdez@PhilaSD-SLA.local>
2021-12-03 13:49:30 -0500

    Initial commit

---
+++ src/edu/sla/Hello.java
@@ -0,0 +1,13 @@
+package edu.sla;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hi Class!  It's Mr. Hernandez.");
+        System.out.println("Sorry for being late.  I hate my son's preschool today.");
+        System.out.println("Who is here?");
+
+
+    }
+}
