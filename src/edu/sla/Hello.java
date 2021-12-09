03f4c8d7d225a7bfb24e1186af4f4246b3456b13
Sean081 <SeanHogan081@gmail.com>
2021-12-09 10:07:26 -0500

    cheeseburger- sean hogan

--- src/edu/sla/Hello.java
+++ src/edu/sla/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
         System.out.println("Sorry for being late.  I hate my son's preschool today.");
         System.out.println("Who is here?");
         System.out.println("Hi, its Tashon");
+        System.out.println("Hello, its me Sean");
 
 
     }
