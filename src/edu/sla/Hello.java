df349badeba4cdb94b60cd104725ac9ad23cc5b5
ariemelvin <ariemelvinwork@gmail.com>
2021-12-09 10:12:38 -0500

    I added my name!!!!!!

--- src/edu/sla/Hello.java
+++ src/edu/sla/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
         System.out.println("NOT Sorry for being late.  I hate my son's preschool today.");
         System.out.println("Who is here?");
         System.out.println("Hi, its Tashon");
-
+        System.out.println("Yo, this is Arie");
 
     }
 }
