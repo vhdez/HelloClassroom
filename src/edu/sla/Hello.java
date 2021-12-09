812e108e33557ef0729d50f6ba79a1232baa2d45
milesmenasion <mmenasion22@scienceleadership.org>
2021-12-09 10:19:48 -0500

    "Hi its Miles"

--- src/edu/sla/Hello.java
+++ src/edu/sla/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
         System.out.println("Who is here?");
         System.out.println("Hi, its Tashon");
         System.out.println("Hello, its me Sean");
-
+        System.out.println("Hi, its Miles.");
 
     }
 }
