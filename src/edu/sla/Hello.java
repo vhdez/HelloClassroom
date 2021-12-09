5626fd045b886c241b0bf0b26b40e91ad1b460ca
vhdez <vhernandez@scienceleadership.org>
2021-12-09 10:09:58 -0500

    I added NOT!

--- src/edu/sla/Hello.java
+++ src/edu/sla/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hi Class!  It's Mr. Hernandez.");
-        System.out.println("Sorry for being late.  I hate my son's preschool today.");
+        System.out.println("NOT Sorry for being late.  I hate my son's preschool today.");
         System.out.println("Who is here?");
         System.out.println("Hi, its Tashon");
 
