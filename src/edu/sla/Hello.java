ee2d79b9d1e62481a7060d5d6fff29729d7da7ab
Triple7islucky <95360528+Triple7islucky@users.noreply.github.com>
2021-12-10 12:43:19 -0500

    Hi it's Terrence

--- src/edu/sla/Hello.java
+++ src/edu/sla/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
         System.out.println("Hi Class!  It's Mr. Hernandez.");
         System.out.println("NOT Sorry for being late.  I hate my son's preschool today.");
         System.out.println("Who is here?");
-        System.out.println("Hi, its Tashon ");
+        System.out.println("Hi, its Tashon");
         System.out.println("Hello, its me Sean ");
         System.out.println("Hi, its Miles ");
         System.out.println("Hi, its Terrence ");
